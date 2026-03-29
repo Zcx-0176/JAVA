775fa41b6536514c3fd5b4be40ae5cd2c5fc835d
zcx <15037861+Cx_Z_01@user.noreply.gitee.com>
2026-03-29 23:21:09 +0800

    练习了JDBC的基本原理

--- java_code/JDBC/src/JdbcUtil.java
+++ java_code/JDBC/src/JdbcUtil.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+import java.sql.*;
 
 public class JdbcUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/jdbc_demo?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456";
+    //加载驱动
+    static{
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+    //获取连接
+    public static Connection getConnection(){
+        try{
+            return DriverManager.getConnection(URL,USER,PASSWORD);
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+    //关闭资源
+    public static void  close(ResultSet rs,Statement stmt,Connection conn){
+        try{
+            if(rs!=null) rs.close();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        try{
+            if(stmt!=null) stmt.close();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        try{
+            if(conn!=null) conn.close();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
+
+
+
+
+
+
+
